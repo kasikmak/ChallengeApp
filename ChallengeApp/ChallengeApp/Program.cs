258fcbf62ba2aa60b61542330c5ba852a18b657a
kasikmak <kasikmak@gmail.com>
2023-02-12 20:59:35 +0100

    Dzień5 wersja 1

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,22 +1,89 @@
-﻿var name = "Kasia";
-var age = 29;
-bool women = true;
+﻿int number = 125569;
+string numberAsString = number.ToString();
+char[] digits = numberAsString.ToArray();
 
-if (women == true)
+/*char[] symbols = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+Console.WriteLine("Ilość cyfr w liczbie:" + number);
+
+foreach (var symbol in symbols)
 {
-    if (age == 33 && name == "Ewa")
+    var counter = 0;
+    foreach (var digit in digits)
     {
-        Console.WriteLine("Ewa, lat 33");
+        if (digit == symbol)
+        {
+            counter++;
+        }
+
     }
-    else if (age < 30)
+
+   Console.WriteLine(counter + "=>" + counter);
+}*/
+
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char digit in digits)
+{
+    if (digit == '0')
+    {   
+        counter0++;
+    }
+    else if (digit == '1')
     {
-        Console.WriteLine("KObieta poniżej 30 lat");
+        counter1++;
+    }
+    else if (digit == '2')
+    {
+        counter2++;
+    }
+    else if (digit == '3')
+    {
+        counter3++;
+    }
+    else if (digit == '4')
+    {
+        counter4++;
+    }
+    else if (digit == '5')
+    {
+        counter5++;
+    }
+    else if (digit == '6')
+    {
+        counter6++;
+    }
+    else if (digit == '7')
+    {
+        counter7++;
+    }
+    else if (digit == '8')
+    {
+        counter8++;
+    }
+    else if (digit == '9')
+    {
+        counter9++;
     }
 }
-else
-{
-    if (age < 18)
-    {
-        Console.WriteLine("Niepełnoletni mężczyzna");
-    }
-}
+    Console.WriteLine("Ilość cyfr w liczbie:" + number);
+    Console.WriteLine("0" + "=>" + counter0);
+    Console.WriteLine("1" + "=>" + counter1);
+    Console.WriteLine("2" + "=>" + counter2);
+    Console.WriteLine("3" + "=>" + counter3);
+    Console.WriteLine("4" + "=>" + counter4);
+    Console.WriteLine("5" + "=>" + counter5);
+    Console.WriteLine("6" + "=>" + counter6);
+    Console.WriteLine("7" + "=>" + counter7);
+    Console.WriteLine("8" + "=>" + counter8);
+    Console.WriteLine("9" + "=>" + counter9);
+
